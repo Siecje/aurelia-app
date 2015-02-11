4841a6ecf2063ae05f132e5160ebcf9796d4110b
Cody Scott <cody.j.b.scott@gmail.com>
2015-02-10 21:07:39 -0500

    Fixing Chart Colours

--- src/report/index.js
+++ src/report/index.js
@@ -64,30 +64,64 @@ export class Flickr{
   }
 
   attached(){
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
     var ctx = document.getElementById("myChart").getContext("2d");
 
     var colors = ["#F7464A", "#46BFBD", "#FDB45C"];
     var highlights = ["#FF5A5E", "#5AD3D1", "#FFC870"];
 
     var data = [];
+    var colour = 0;
     for(var i=0;i<this.models.expenses.length;i++){
-      data.push({value: this.models.expenses[i].amount, color: colors[i], highlight: highlights[i], label: this.models.expenses[i].name});
+      colour = getRandomColor();
+      data.push({value: this.models.expenses[i].amount, color: colour, highlight: colour, label: this.models.expenses[i].name});
     }
     for(var i=0;i<this.models.bills.length;i++){
-      data.push({value: this.models.bills[i].amount, color: colors[i], highlight: highlights[i], label: this.models.bills[i].name});
+      colour = getRandomColor();
+      data.push({value: this.models.bills[i].amount, color: colour, highlight: colour, label: this.models.bills[i].name});
     }
     for(var i=0;i<this.models.creditCards.length;i++){
-      data.push({value: this.models.creditCards[i].payment, color: colors[i], highlight: highlights[i], label: this.models.creditCards[i].name});
+      colour = getRandomColor();
+      data.push({value: this.models.creditCards[i].payment, color: colour, highlight: colour, label: this.models.creditCards[i].name});
     }
     for(var i=0;i<this.models.overdrafts.length;i++){
-      data.push({value: this.models.overdrafts[i].monthlyFee + this.models.overdrafts[i].amount*(this.models.overdrafts[i].interest/12), color: colors[i], highlight: highlights[i], label: this.models.overdrafts[i].name});
+      colour = getRandomColor();
+      data.push({value: this.models.overdrafts[i].monthlyFee + this.models.overdrafts[i].amount*(this.models.overdrafts[i].interest/12), color: colour, highlight: colour, label: this.models.overdrafts[i].name});
     }
     // index in forEach?
     //this.models.expenses.forEach(expense => data.push({value: expense.amount, color: colors[i], highlight: highlights[i], label: expense.name}))
 
-
     //var myNewChart = new Chart(ctx).Line(data);
-    var myDoughnutChart = new Chart(ctx).Doughnut(data);
-  }
+    //var myDoughnutChart = new Chart(ctx).Doughnut(data);
 
+    var moduleDoughnut = new Chart(ctx).Doughnut(data, { tooltipTemplate : "<%if (label){%><%=label%>: <%}%><%= value %>kb", animation: false });
+
+    // var canvas = document.getElementById('myChart');
+    //
+    // var helpers = Chart.helpers;
+    // var legendHolder = document.createElement('div');
+    // legendHolder.innerHTML = moduleDoughnut.generateLegend();
+    // // Include a html legend template after the module doughnut itself
+    // helpers.each(legendHolder.firstChild.childNodes, function(legendNode, index){
+    //   helpers.addEvent(legendNode, 'mouseover', function(){
+    //     var activeSegment = moduleDoughnut.segments[index];
+    //     activeSegment.save();
+    //     activeSegment.fillColor = activeSegment.highlightColor;
+    //     moduleDoughnut.showTooltip([activeSegment]);
+    //     activeSegment.restore();
+    //   });
+    // });
+    // helpers.addEvent(legendHolder.firstChild, 'mouseout', function(){
+    //   moduleDoughnut.draw();
+    // });
+    // canvas.parentNode.parentNode.appendChild(legendHolder.firstChild);
+  }
 }
